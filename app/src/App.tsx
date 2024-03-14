c19d9460e88e8a8df546556bbddfc02c3a62f31c
Karoline <karoline.biancardi010@gmail.com>
2024-03-14 16:21:20 -0300

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="container h-screen w-screen mx-auto flex items-center justify-center">
+   <h1>heyy</h1>
+   </div>
+  )
+}
+
+export default App
